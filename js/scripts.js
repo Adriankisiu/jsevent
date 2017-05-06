db407b5a062fa79828e6c05901fc863c1203af58
Adrian <tommazost@gmail.com>
2017-05-06 11:44:57 +0200

    event

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+var list = document.getElementById('list'),
+	add = document.getElementById('addElem');
+
+list.addEventListener('click', function(e) {
+	var insert = document.getElementsByTagName("li");
+	var changeinsert = insert[1].innerHTML += '0';
+});
+
+add.addEventListener('click', function(e) {
+  list.innerHTML +='<li>item</li>';
+});
+
